0873fdc626d04978cc0224f14aeff924f8f779b0
Will Murphy <william@datatitian.com>
2021-05-18 23:16:16 -0500

    also update compose files when updating

--- immers/update.sh
+++ immers/update.sh
@@ -4,6 +4,7 @@
 docker-compose exec -T mongodb sh -c 'mongodump --archive' > immersdb.dump
 
 # update and restart
+git pull
 docker-compose pull
 docker-compose up -d --remove-orphans
 docker image prune -f
